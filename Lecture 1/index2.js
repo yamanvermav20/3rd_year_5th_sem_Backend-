8de06a5108e9ebea8e36d294a598e00af222a59b
yamanvermav20 <yamanverma083@gmail.com>
2025-07-03 15:45:59 +0530

    Lec-1

---
+++ Lecture 1/index2.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+console.log(fs);
+console.log("start");
+
+setImmediate(() =>{
+    console.log("set Immediate");
+})
+
+setTimeout(() => {
+    console.log("settimeout");
+}, 1)
+fs.readFile("demo.txt", "utf-8", (err, data) => {
+    console.log(data)
+    setTimeout(() => {
+        console.log("timer 2")
+    }, 0)
+    setImmediate(() => {
+        console.log("immediate 2");
+    })
+});
+
+function someTask(){
+    return new Promise((resolve, reject) => {
+        resolve("promise")
+    })
+}
+someTask().then((data) => {
+    console.log(data)
+})
+.catch((err) => {
+    console.log(err)
+})
+process.nextTick(() => {
+    console.log("next tick")
+})
+console.log("end");
