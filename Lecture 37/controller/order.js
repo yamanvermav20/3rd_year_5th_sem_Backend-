40ae6d622464cfcd51edc7e2ff2ddd3cd109ceab
yamanvermav20 <yamanverma083@gmail.com>
2025-11-11 12:39:11 +0530

    Lecture 37

--- Lecture 37/controller/order.js
+++ Lecture 37/controller/order.js
@@ -1,6 +1,10 @@
+const OrderBook = require("../service/orderBook");
+const ob = new OrderBook("BTCUSD");
+
 module.exports.postPlaceOrder = async(req, res) => {
     let { type, side, price, quantity, username, symbol } = req.body;
     //basic validation
 
-    let response = 
+    let response = OrderBook.placeOrder(price, quantity, type, side, username);
+    console.log(response);
 }
